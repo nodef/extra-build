1183c18ea0949c6add4fdda7bb6a8cf1e3fb3379
wolfram77 <wolfram77@gmail.com>
2020-04-08 21:13:49 +0530

    -bundle

--- src/dirScatter.js
+++ src/dirScatter.js
@@ -5,11 +5,21 @@ const snakeCase = require('./snakeCase');
 const path = require('path');
 const cp = require('child_process');
 
+
+const ORG = 'nodef';
+const PACKAGE_ROOT = require('./package.json').name;
+const STANDALONE_ROOT = PACKAGE_ROOT.replace(/extra-/, '').replace(/\W+/, '_');
+const OPTIONS = {
+  org: ORG,
+  package_root: PACKAGE_ROOT,
+  standalone_root: STANDALONE_ROOT
+};
 const stdio = [0, 1, 2];
 
 
 function dirScatter(pth, o) {
   console.log('dirScatter:', pth, o);
+  var o = Object.assign({}, OPTIONS, o);
   for(var f of fsReadDirSync(pth)) {
     if(path.extname(f)!=='.js') continue;
     if(f.startsWith('_')) continue;
