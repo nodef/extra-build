bed2f8b0aac9a68fb5a3a26c6c07c15ccc64bd0d
wolfram77 <wolfram77@gmail.com>
2020-04-09 00:13:10 +0530

    fix: mdHeading allows *

--- src/mdHeading.js
+++ src/mdHeading.js
@@ -5,7 +5,7 @@ function mdHeading(pth='README.md') {
   console.log('mdHeading:', pth);
   var d = fs.readFileSync(pth, 'utf8');
   d = d.replace(/\r?\n[\s\S]*/, '');
-  d = d.replace(/[\_\*\[\]]/g, '');
+  d = d.replace(/[\_\[\]]/g, '');
   d = d.replace(/\*(.*?)\*/g, '$1');
   return d;
 }
