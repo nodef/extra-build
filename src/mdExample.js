db7c8ba4273402fed84555e7a1f41f3b7949bcb6
wolfram77 <wolfram77@gmail.com>
2020-04-10 04:40:12 +0530

    src: example try strip comments

--- src/mdExample.js
+++ src/mdExample.js
@@ -1,8 +1,9 @@
 const mdCodeBlocks = require('./mdCodeBlocks');
-
+const stripComments = require('strip-comments');
 
 function mdExample(x, re) {
   var bs = mdCodeBlocks(x, re);
-  return bs.length>1? bs[1] : bs[0];
+  var ex = bs.length>1? bs[1] : (bs[0]||'');
+  return stripComments(ex);
 }
 module.exports = mdExample;
