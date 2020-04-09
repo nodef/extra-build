3359cf0bc210300954a18faebd499f80cecea037
wolfram77 <wolfram77@gmail.com>
2020-04-10 04:51:40 +0530

    src: example try strip comments

--- src/mdExample.js
+++ src/mdExample.js
@@ -4,6 +4,6 @@ const stripComments = require('strip-comments');
 function mdExample(x, re) {
   var bs = mdCodeBlocks(x, re);
   var ex = bs.length>1? bs[1] : (bs[0]||'');
-  return stripComments(ex).replace(/(\r?\n)(\r?\n)+/g, '$1$2');
+  return stripComments(ex).replace(/(\r?\n)(\r?\n)+/g, '$1$2').trim()+'\n';
 }
 module.exports = mdExample;
