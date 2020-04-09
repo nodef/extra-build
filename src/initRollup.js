2a980f8f6a2dd90fbe0e5eaa59d13b9ee9fabbf4
wolfram77 <wolfram77@gmail.com>
2020-04-10 03:57:10 +0530

    src: fix cant read js as json

--- src/initRollup.js
+++ src/initRollup.js
@@ -1,6 +1,4 @@
 const DIRBUILD = require('./DIRBUILD');
-const jsonRead = require('./jsonRead');
-const jsonWrite = require('./jsonWrite');
 const path = require('path');
 const fs = require('fs');
 
@@ -11,8 +9,8 @@ const FILEROLLUP = path.join(DIRBUILD, 'data', 'rollup.config.js');
 function initRollup(pth, o) {
   var pth = pth||'rollup.config.js', o = o||{};
   if(fs.existsSync(pth)) return;
-  o.rollup_config = o.rollup_config||jsonRead(FILEROLLUP);
+  o.rollup_config = o.rollup_config||fs.readFileSync(FILEROLLUP, 'utf8');
   console.log('initRollup:', pth, o);
-  jsonWrite(pth, o.rollup_config);
+  fs.writeFileSync(pth, o.rollup_config);
 }
 module.exports = initRollup;
