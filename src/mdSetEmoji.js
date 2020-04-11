0627888a80ab34bce19086ceadd1fd778303b5ae
wolfram77 <wolfram77@gmail.com>
2020-04-11 22:06:14 +0530

    next try updateEmoji

--- src/mdSetEmoji.js
+++ src/mdSetEmoji.js
@@ -7,12 +7,13 @@ const RUN = ':running:';
 
 function mdSetEmoji(md, o) {
   var p = packageName(o.symbol, o);
-  var run = `[${RUN}](https://npm.runkit.com/${p})`;
-  var pkg = `[${PKG}](https://www.npmjs.com/package/${p})`;
-  var min = `[${MIN}](https://www.npmjs.com/package/${p}.min)`;
-  md = md.replace(/^(.*?\.?).*?\n/, `$1 [${RUN}] [${PKG}] [${MIN}]\n`);
+  var run = `[${RUN}]: https://npm.runkit.com/${p}`;
+  var pkg = `[${PKG}]: https://www.npmjs.com/package/${p}`;
+  var min = `[${MIN}]: https://www.npmjs.com/package/${p}.min`;
+  md = md.replace(/^([^\.\n]*\.?).*?\n/, `$1 [${RUN}] [${PKG}] [${MIN}]\n`);
   if(!md.includes(run)) md += run+'\n';
   if(!md.includes(pkg)) md += pkg+'\n';
   if(!md.includes(min)) md += min+'\n';
+  return md;
 }
 module.exports = mdSetEmoji;
