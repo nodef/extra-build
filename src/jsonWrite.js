a093bed122fecdc9920ba407c3c35d7a60839e13
wolfram77 <wolfram77@gmail.com>
2020-04-11 20:44:05 +0530

    next test bin

--- src/jsonWrite.js
+++ src/jsonWrite.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
+const os = require('os');
+
+const {EOL} = os;
 
 
 function jsonWrite(pth, v) {
   var pth = pth||'package.json';
-  var d = JSON.stringify(v, null, 2);
+  var d = JSON.stringify(v, null, 2)+EOL;
   fs.writeFileSync(pth, d);
 }
 module.exports = jsonWrite;
