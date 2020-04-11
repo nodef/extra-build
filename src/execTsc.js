4039693c61f28f14ae56c79bba44e643a760b87c
wolfram77 <wolfram77@gmail.com>
2020-04-11 16:13:23 +0530

    next test execTsc

--- src/execTsc.js
+++ src/execTsc.js
@@ -12,16 +12,17 @@ const OPTIONS = {
 
 
 function execTsc(pth, o) {
-  var pth = pth||'index.ts', o = o||{};
-  var build = o.build||'tsconfig.json';
-  var o = Object.assign({}, fs.existsSync(build)? {build}:OPTIONS, o);
+  var pth = pth||'index.ts';
+  var {build} = Object.assign({build: 'tsconfig.json'}, o);
+  var hasBuild = fs.existsSync(build);
+  var o = Object.assign({}, hasBuild? {build}:OPTIONS, o);
   console.log('execTsc:', pth, o);
   var cwd = packageRoot(pth), cmd = '.tsc';
   for(var k in o) {
     if(typeof k==='boolean') cmd += ` --${k}`;
     else cmd += ` --${k} "${o[k]}"`;
   }
-  cmd += ` "${pth}"`;
+  if(!hasBuild) cmd += ` "${pth}"`;
   cpExec(cmd, {cwd});
 }
 module.exports = execTsc;
