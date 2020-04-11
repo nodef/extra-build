3547ba83c736ca32b0c67278cde8858bd7c79ecc
wolfram77 <wolfram77@gmail.com>
2020-04-11 17:48:57 +0530

    next test update

--- src/jsdocParse.js
+++ src/jsdocParse.js
@@ -1,6 +1,6 @@
 function jsdocParse(c) {
   var description = c.match(/\s+\*\s+(.*?)\n/)[1];
-  var rparam = /\s+\*\s+@param\s+(?:\{(.*?)\}\s+)(.*?)\s+(.*?)\n/g;
+  var rparam = /\s+\*\s+@param\s+(?:\{(.*?)\}\s+)?(.*?)\s+(.*?)\n/g;
   var params = new Map(), m = null;
   while((m=rparam.exec(c))!=null) {
     params.set(m[2], {type: m[1], description: m[3]});
@@ -8,7 +8,7 @@ function jsdocParse(c) {
     var k = m[2].replace(/\..*/, '');
     params.get(k).type += '?';
   }
-  var rreturns = /\s+\*\s+@returns\s+(?:\{(.*?)\}\s+)(.*?)\n/;
+  var rreturns = /\s+\*\s+@returns\s+(?:\{(.*?)\}\s+)?(.*?)\n/;
   var m = rreturns.exec(c);
   var returns = m? {type: m[1], description: m[2]}:null;
   return {description, params, returns};
