4584ed5f774751229cd8215eb0b0d9b870fe1bf6
wolfram77 <wolfram77@gmail.com>
2020-04-11 19:02:23 +0530

    next test updateWiki

--- src/updateReadme.js
+++ src/updateReadme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 function updateReadme(pth, jsdocs, o) {
   var pth = pth||'README.md';
-  console.log('updateReadme:', pth, jsdocs.size(), o);
+  console.log('updateReadme:', pth, o);
   var md = fs.readFileSync(pth, 'utf8');
   md = mdSetTable(md, jsdocs);
   md = mdSetLinks(md, o);
