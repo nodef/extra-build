70f3d2b1edb70735321476c12ed92efb03d16d25
wolfram77 <wolfram77@gmail.com>
2020-04-11 15:25:41 +0530

    next test execDts

--- src/execDts.js
+++ src/execDts.js
@@ -7,18 +7,19 @@ const {EOL} = os;
 
 
 function execDts(pth, o) {
-  var pth = pth||'index.d.ts';
-  var o = Object.assign({outFile: pth, noBanner: true}, o);
-  var cmd = '.dts-bundle-generator';
+  var pth = pth||'src/index.ts';
+  var o = Object.assign({outFile: 'index.d.ts', noBanner: true}, o);
+  var cmd = '.dts-bundle-generator', dts = o.outFile;
   for(var k in o) {
     var f = snakeCase(k);
     if(typeof o[k]==='boolean') cmd += ` --${f}`;
     else cmd += ` --${f} "${o[k]}"`;
   }
+  cmd += ` "${pth}"`;
   cpExec(cmd);
   if(!o.package_root) return;
-  var d = fs.readFileSync(pth, 'utf8');
+  var d = fs.readFileSync(dts, 'utf8');
   d = `declare module '${o.package_root}' {`+EOL+d+`}`+EOL;
-  fs.writeFileSync(pth, d);
+  fs.writeFileSync(dts, d);
 }
 module.exports = execDts;
