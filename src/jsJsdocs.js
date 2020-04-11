bd8e3f3349b3889733ca3f2adfa010a7b54d2a7f
wolfram77 <wolfram77@gmail.com>
2020-04-11 18:40:40 +0530

    next test updateWiki

--- src/jsJsdocs.js
+++ src/jsJsdocs.js
@@ -7,7 +7,7 @@ function jsJsdocs(js) {
   var rjsdoc = /(\/\*\*.*?\*\/).*?(function\*?|const|var|let)\s+([\w$]+)([^\{]*)/gs, m = null;
   while((m=rjsdoc.exec(js))!=null) {
     var [, com,, name, def] = m;
-    a.set(name, jsdocParse(com, def));
+    a.set(name, jsdocParse(com, def||''));
   }
   return a;
 }
