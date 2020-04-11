7b3d33a60b3cd78a31eaf1d7a64fbdaeade7a5cf
wolfram77 <wolfram77@gmail.com>
2020-04-11 18:40:14 +0530

    next test updateWiki

--- src/jsJsdocs.js
+++ src/jsJsdocs.js
@@ -4,10 +4,10 @@ const jsdocParse = require('./jsdocParse');
 // Reads JSDocs in js file.
 function jsJsdocs(js) {
   var a = new Map();
-  var rjsdoc = /(\/\*\*.*?\*\/).*?(function\*?|const|var|let)\s+([\w$]+)/gs, m = null;
+  var rjsdoc = /(\/\*\*.*?\*\/).*?(function\*?|const|var|let)\s+([\w$]+)([^\{]*)/gs, m = null;
   while((m=rjsdoc.exec(js))!=null) {
-    var [, jsdoc,, name] = m;
-    a.set(name, jsdocParse(jsdoc));
+    var [, com,, name, def] = m;
+    a.set(name, jsdocParse(com, def));
   }
   return a;
 }
