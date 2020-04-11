92aea683f2ee41178ee4fd8bb7db2757727d23a4
wolfram77 <wolfram77@gmail.com>
2020-04-11 17:36:38 +0530

    next test update

--- src/update.js
+++ src/update.js
@@ -13,7 +13,14 @@ const updateMain = require('./updateMain');
 const OPTIONS = {
   org: ORG,
   package_root: PACKAGE,
-  symbol_root: SYMBOL
+  symbol_root: SYMBOL,
+  json: true,
+  readme: true,
+  example: true,
+  wiki: true,
+  exports: true,
+  main: true,
+  github: true
 };
 
 
@@ -21,12 +28,12 @@ async function update(o) {
   var o = Object.assign({}, OPTIONS, o);
   console.log('update:', o);
   var jsdocs = dirJsdocs();
-  updateJson(o.json_path, o);
-  updateReadme(o.readme_path, jsdocs, o);
-  updateExample(o.example_path, o);
-  updateWiki(o.wiki_path, jsdocs, o);
-  updateExports(o.exports_path, o);
-  updateMain(o.main_path, o);
-  await updateGithub(o);
+  if(o.json) updateJson(o.json_path, o);
+  if(o.readme) updateReadme(o.readme_path, jsdocs, o);
+  if(o.example) updateExample(o.example_path, o);
+  if(o.wiki) updateWiki(o.wiki_path, jsdocs, o);
+  if(o.exports) updateExports(o.exports_path, o);
+  if(o.main) updateMain(o.main_path, o);
+  if(o.github) await updateGithub(o);
 };
 module.exports = update;
