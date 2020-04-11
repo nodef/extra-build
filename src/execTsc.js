0cd9f74e0f9472779acfa1e834da23061bce7daa
wolfram77 <wolfram77@gmail.com>
2020-04-11 16:08:04 +0530

    next test execTsc

--- src/execTsc.js
+++ src/execTsc.js
@@ -3,30 +3,25 @@ const packageRoot = require('./packageRoot');
 const fs = require('fs');
 
 const OPTIONS = {
-  config: fs.existsSync('tsconfig.json'),
   target: 'es2018',
   module: 'es2015',
   declaration: true,
   declarationMap: true,
-  sourceMap: true,
-  outFile: null,
-  outDir: null
+  sourceMap: true
 };
 
 
 function execTsc(pth, o) {
-  var pth = pth||'index.ts';
-  var o = Object.assign({}, OPTIONS, o);
+  var pth = pth||'index.ts', o = o||{};
+  var build = o.build||'tsconfig.json';
+  var o = Object.assign({}, fs.existsSync(build)? {build}:OPTIONS, o);
   console.log('execTsc:', pth, o);
-  var cwd = packageRoot(pth);
-  if(o.config) cpExec(`.tsc --build tsconfig.json`, {cwd});
-  else {
-    var cmd = `.tsc --target ${o.target} --module ${o.module}`;
-    if(o.declaration) cmd += ` --declaration`;
-    if(o.declarationMap) cmd += ` --declarationMap`;
-    if(o.sourceMap) cmd += ` --sourceMap`;
-    cmd += ` "${pth}"`;
-    cpExec(cmd, {cwd});
+  var cwd = packageRoot(pth), cmd = '.tsc';
+  for(var k in o) {
+    if(typeof k==='boolean') cmd += ` --${k}`;
+    else cmd += ` --${k} "${o[k]}"`;
   }
+  cmd += ` "${pth}"`;
+  cpExec(cmd, {cwd});
 }
 module.exports = execTsc;
--- src/updateExports.js
+++ src/updateExports.js
@@ -6,20 +6,20 @@ const fs = require('fs');
 const os = require('os');
 
 const OPTIONS = {
-  format: 'cjs'
+  format: 'es'
 };
 const {EOL} = os;
 
 
 function updateExports(pth, o) {
-  var pth = pth||'src/index.js';
+  var pth = pth||'src/index.ts';
   var o = Object.assign({}, OPTIONS, o);
   console.log('updateExports:', o, pth);
   var dir = path.dirname(pth), d = '';
   for(var f of dirFiles(dir)) {
     var file = fileName(f);
     var symbol = fileSymbol(f);
-    if(o.format==='cjs') d += `exports.${symbol} = require('./${file}');`+EOL;
+    if(o.format!=='es') d += `exports.${symbol} = require('./${file}');`+EOL;
     else d += `export {default as ${symbol}} from './${file}';`+EOL;
   }
   fs.writeFileSync(pth, d);
