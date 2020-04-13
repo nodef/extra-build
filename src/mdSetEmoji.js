019f4b8ac83376bca2a4cd5e7eadbb3828d2506b
wolfram77 <wolfram77@gmail.com>
2020-04-13 06:30:38 +0530

    next test asciinema

--- src/mdSetEmoji.js
+++ src/mdSetEmoji.js
@@ -9,7 +9,7 @@ const MIN = ':moon:';
 
 function mdSetEmoji(md, o) {
   var p = packageName(o.symbol, o);
-  var rcin = /\[:cin:\]\: (.*)\n/, m = md.exec(rcin);
+  var rcin = /\[:cin:\]\: (.*)\n/, m = rcin.exec(md);
   if(o.diff_code_blocks) md = md.replace(rcin, '');
   var run = `[${RUN}]: https://npm.runkit.com/${p}`;
   var cin = `[${CIN}]: ${m && !o.diff_code_blocks? m[1] : mdAsciinema(md)}`;
