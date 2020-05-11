dc9fd5b04587e721f3cba7deb6ad3c7ac8dc6583
Subhajit Sahu <wolfram77@gmail.com>
2020-05-11 11:49:54 +0530

    hank up a new jsExportsDefine()

---
+++ src/jsExportsDefine.js
@@ -0,0 +1,5 @@
+function jsExportsDefine(sym, pth, fmt='es') {
+  if(fmt==='es') return `export {default as ${sym}} from './${pth}';`;
+  return `exports.${sym} = require('./${pth}');`;
+}
+module.exports = jsExportsDefine;
--- src/updateExports.js
+++ src/updateExports.js
@@ -3,6 +3,8 @@ const fileName = require('./fileName');
 const fileSymbol = require('./fileSymbol');
 const fileRead = require('./fileRead');
 const jsExports = require('./jsExports');
+const jsExportsDefine = require('./jsExportsDefine');
+const jsExportsRemove = require('./jsExportsRemove');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
@@ -18,17 +20,14 @@ function updateExports(pth, o) {
   var pth = pth||`src/index${ext}`;
   var o = Object.assign({}, OPTIONS, o);
   console.log('updateExports:', pth, o);
-  var d = fileRead(pth);
-  d = d.replace(/exports\.\S+ = require\(\'\.\/.*?\n/g, '');
-  d = d.replace(/export \{default as \S+\} from \'\..*?\n/g, '');
+  var d = jsExportsRemove(fileRead(pth));
   var symbols = jsExports(d);
   var dir = path.dirname(pth);
   for(var f of dirFiles(dir)) {
     var file = fileName(f);
     var symbol = fileSymbol(f);
     if(symbols.has(symbol)) { console.log('updateExports: skipping symbol '+symbol); continue; }
-    if(o.format!=='es') d += `exports.${symbol} = require('./${file}');`+EOL;
-    else d += `export {default as ${symbol}} from './${file}';`+EOL;
+    d += jsExportsDefine(symbol, file, o.format)+EOL;
   }
   fs.writeFileSync(pth, d);
 }
