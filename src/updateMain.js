70906ab6db1c7bc1eaa141549c1d2e94bfa6b143
Subhajit Sahu <wolfram77@gmail.com>
2020-05-11 12:38:25 +0530

    hank needs to gen dts from a different file

--- src/updateMain.js
+++ src/updateMain.js
@@ -2,6 +2,7 @@ const execTsc = require('./execTsc');
 const execRollup = require('./execRollup');
 const execDts = require('./execDts');
 const pathSplit = require('./pathSplit');
+const pathReplaceExt = require('./pathReplaceExt');
 const jsDecomment = require('./jsDecomment');
 const path = require('path');
 const fs = require('fs');
@@ -14,11 +15,13 @@ var OPTIONS = {
 
 function updateMain(pth, o) {
   var pth = pth||'src/index.ts';
+  var ext = path.extname(pth);
+  var dec = pathReplaceExt(pth, '.d'+ext);
   var o = Object.assign({}, OPTIONS, o);
   console.log('updateMain:', pth, o);
   execTsc(pth, o.tsc);
   execRollup(o.build, o.rollup);
-  execDts(pth, o.dts);
+  execDts(dec, o.dts);
   var js1 = o.output;
   var [dir, fil,] = pathSplit(js1);
   var dts1 = path.join(dir, fil+'.d.ts');
