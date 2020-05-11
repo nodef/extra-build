467d7169a591c174e0f9149b90d5273ed2b5d97d
Subhajit Sahu <wolfram77@gmail.com>
2020-05-11 18:04:09 +0530

    :detective: hank needs lissting

--- src/mdSetEmoji.js
+++ src/mdSetEmoji.js
@@ -4,6 +4,7 @@ const RUN = ':running:';
 const CIN = ':vhs:';
 const PKG = ':package:';
 const MIN = ':moon:';
+const LST = ':ledger:';
 
 
 function mdSetEmoji(md, o) {
@@ -14,11 +15,13 @@ function mdSetEmoji(md, o) {
   var cin = `[${CIN}]: ${m && !o.diff_code_blocks? m[1] : mdAsciinema(md, o)}`;
   var pkg = `[${PKG}]: https://www.npmjs.com/package/${p}`;
   var min = `[${MIN}]: https://www.npmjs.com/package/${p}.min`;
-  md = md.replace(/^([^\.\n]*\.?).*?\n/, `$1 [${RUN}] [${CIN}] [${PKG}] [${MIN}]\n`);
+  var lst = `[${LST}]: https://unpkg.com/${p}/`;
+  md = md.replace(/^([^\.\n]*\.?).*?\n/, `$1 [${RUN}] [${CIN}] [${PKG}] [${MIN}] [${LST}]\n`);
   if(!md.includes(run)) md += run+'\n';
   if(!md.includes(cin)) md += cin+'\n';
   if(!md.includes(pkg)) md += pkg+'\n';
   if(!md.includes(min)) md += min+'\n';
+  if(!md.includes(lst)) md += lst+'\n';
   return md;
 }
 module.exports = mdSetEmoji;
