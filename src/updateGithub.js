76c737cd94541c0e122c5778df7d371c007a4664
Subhajit Sahu <wolfram77@gmail.com>
2020-06-01 00:55:19 +0530

    :watermelon: cool fruit save

--- src/updateGithub.js
+++ src/updateGithub.js
@@ -2,10 +2,12 @@ const githubRepoUpdate = require('./githubRepoUpdate');
 const githubTopicsUpdate = require('./githubTopicsUpdate');
 
 
-async function updateGithub(o) {
+function updateGithub(o) {
   var o = o||{};
   console.log('updateGithub:', o);
-  await githubRepoUpdate(o);
-  await githubTopicsUpdate(o);
+  return Promise.all([
+    githubRepoUpdate(o),
+    githubTopicsUpdate(o)
+  ]);
 }
 module.exports = updateGithub;
