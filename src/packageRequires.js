46ebb695dd11cbbc2823f5f0ccac8a3e0e5ef30a
Subhajit Sahu <wolfram77@gmail.com>
2020-05-31 23:42:25 +0530

    :watermelon: cool fruit save

--- src/packageRequires.js
+++ src/packageRequires.js
@@ -5,14 +5,16 @@ const path = require('path');
 
 // Gets requires from code.
 function packageRequires(pth, a=new Set()) {
-  var p = requireResolve(pth); a.add('./'+p);
+  var p = requireResolve(pth);
+  if(a.has('./'+p)) return a;
+  else a.add('./'+p);
   var d = fs.readFileSync(p, 'utf8');
   var re = /require\(\'(.*?)\'\)|^(?:import|export).*?from\s*\'(.*?)\';?$/gm, m = null;
   for(var reqs=[]; (m=re.exec(d))!=null;)
     reqs.push(m[1]||m[2]);
   var dir = path.dirname(pth);
   for(var p of reqs) {
-    if(/^\./.test(p) && !a.has(p)) packageRequires(path.join(dir, p), a);
+    if(/^\./.test(p)) packageRequires(path.join(dir, p), a);
     else a.add(p);
   }
   return a;
