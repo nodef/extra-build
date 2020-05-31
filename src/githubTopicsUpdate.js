9ff1e6adad7a1f85d194edbbb2eabcb721840938
Subhajit Sahu <wolfram77@gmail.com>
2020-06-01 00:52:40 +0530

    :watermelon: cool fruit save

--- src/githubTopicsUpdate.js
+++ src/githubTopicsUpdate.js
@@ -10,12 +10,11 @@ async function githubTopicsUpdate(o) {
   var repo = o.package_root||PACKAGE;
   var {names} = (await octokit.repos.getAllTopics({owner, repo})).data;
   var keywords_min = o.keywords_min||10;
-  if(names.length<keywords_min) {
-    var names = o.keywords||jsonRead().keywords;
-    names.length = Math.min(names.length, keywords_min);
-    var c = {owner, repo, names};
-    console.log('githubTopicsUpdate:', c);
-    await octokit.repos.replaceAllTopics(c);
-  }
+  var names = o.keywords||jsonRead().keywords;
+  names.length = Math.min(names.length, keywords_min);
+  names = names.map(n => n.toLowerCase().replace(/_/g, '-').replace(/[^\w-]/g, ''));
+  var c = {owner, repo, names};
+  console.log('githubTopicsUpdate:', c);
+  await octokit.repos.replaceAllTopics(c);
 }
 module.exports = githubTopicsUpdate;
