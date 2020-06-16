bdb284c27ba54920f9833b2c910d761c967473e2
Subhajit Sahu <wolfram77@gmail.com>
2020-06-16 13:57:41 +0530

    :watermelon: cool fruit considers export {name} too

--- src/jsExports.js
+++ src/jsExports.js
@@ -2,7 +2,7 @@
 function jsExports(d) {
   var a = new Set(), m = null;
   var rjs = /exports\.(\S+)\s*=/g;
-  var res = /export\s+\{\s*\S+\s+as\s+(\S+)\s*\}/g;
+  var res = /export\s+\{\s*[(\S+)(?:\S+\s+as\s+(\S+))]\s*\}/g;
   while((m=rjs.exec(d))!=null) a.add(m[1]);
   while((m=res.exec(d))!=null) a.add(m[1]);
   return a;
