7b92ffc9ff6f7edf89e85e0483ad32f442c0b8c2
Subhajit Sahu <wolfram77@gmail.com>
2020-06-29 21:03:19 +0530

    :coconut: chutney mdLinkWikis: x is not defined

--- src/mdLinkWikis.js
+++ src/mdLinkWikis.js
@@ -5,15 +5,15 @@ const wikiLink = require('./wikiLink');
 
 const RWIKI = /github\.com.*?\/wiki\//;
 
-function mdLinkWikis(md, o) {
-  var ls = mdLinks(md);
-  md = mdFilterHrefs(md, (v, k) => {
+function mdLinkWikis(x, o) {
+  var ls = mdLinks(x);
+  x = mdFilterHrefs(x, (v, k) => {
     if(!ls.has(k)) return false;
     if(!RWIKI.test(v)) ls.delete(k);
     return true;
   });
   for(var k of ls)
-    md = mdSetHref(x, k, wikiLink(k, o));
-  return md;
+    x = mdSetHref(x, k, wikiLink(k, o));
+  return x;
 }
 module.exports = mdLinkWikis;
