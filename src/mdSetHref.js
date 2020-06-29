974dd14f88c1739a5e5e496068fcacf08cb6961f
Subhajit Sahu <wolfram77@gmail.com>
2020-06-29 21:18:15 +0530

    :coconut: chutney duplicate additions

--- src/mdSetHref.js
+++ src/mdSetHref.js
@@ -3,9 +3,9 @@ const {EOL} = require('os');
 const RLAST = /(\r?\n)(\r?\n)+$|\[.*?\]:\s+.*?\n$/;
 
 function mdSetHref(x, k, v) {
-  var re = new RegExp(`\\[${k}\\]:\\s+[^\\r\\n]\r?\n`, 'g'), has = false;
+  var re = new RegExp(`\\[${k}\\]:\\s+([^\\r\\n]+)\r?\n`, 'g'), has = false;
   x = x.replace(re, (m, p1) => {
-    if(!has || p1===v) { has = true; return m; }
+    if(!has && p1===v) { has = true; return m; }
     console.log('mdSetHref: remove', k, v); return '';
   });
   if(has || v==null) return x;
