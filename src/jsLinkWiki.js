e024691778dd1b039606c556c0e5a3fd38481751
Subhajit Sahu <wolfram77@gmail.com>
2020-06-29 23:50:18 +0530

    :coconut: chutney not optional

--- src/jsLinkWiki.js
+++ src/jsLinkWiki.js
@@ -2,7 +2,7 @@ const wikiLink = require('./wikiLink');
 
 // Add more link to jsdoc.
 function jsLinkWiki(js, o) {
-  var re = /(\/\*\*.*?\*\/).*?((export\s+)(?:(function\*?|class|const|var|let)\s+)?([\w$]+)([^\{;]*))/gs;
+  var re = /(\/\*\*.*?\*\/).*?((export\s+)?(?:(function\*?|class|const|var|let)\s+)?([\w$]+)([^\{;]*))/gs;
   var rdesc = /(\s+\*\s+)(.*?)(\r?\n)/, cls = '';
   return js.replace(re, (m, com, exp, def, typ, nam, arg) => {
     if(!exp) return m;
