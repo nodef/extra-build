1799eebf3f9d88bb26ec5beaf4053a2bbdbfe296
Subhajit Sahu <wolfram77@gmail.com>
2020-06-30 00:13:32 +0530

    :coconut: chutney was all right

--- src/mdLinkWikis.js
+++ src/mdLinkWikis.js
@@ -4,21 +4,14 @@ const mdFilterHrefs = require('./mdFilterHrefs');
 const wikiLink = require('./wikiLink');
 
 const RWIKI = /github\.com.*?\/wiki\//;
-var i = 0;
 
 function mdLinkWikis(x, o) {
   var ls = mdLinks(x), ws = new Set();
-  if(i===0) console.log(ws);
   x = mdFilterHrefs(x, (v, k) => {
     if(!ls.has(k)) return false;
-    if(RWIKI.test(v)) {
-      if(i===0) console.log(k, v);
-      ws.add(k);
-    }
+    if(RWIKI.test(v)) ws.add(k);
     return true;
   });
-  if(i===0) console.log(ws);
-  i++;
   for(var k of ws)
     x = mdSetHref(x, k, wikiLink(k, o));
   return x;
