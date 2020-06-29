8e4a9fa68b34c0c6a9891dc55d3a13a8c8d808f3
Subhajit Sahu <wolfram77@gmail.com>
2020-06-30 00:53:12 +0530

    :coconut: chutney test

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -4,6 +4,7 @@ function mdLinks(x, dir=false) {
   var a = new Set(), m = null;
   var x = x.replace(/```.*?```/gs, '');
   while((m=RLINK.exec(x))!=null) {
+    console.log(m.slice(1, 4));
     if(m[1]==='!' || m[3]===':') continue;
     if(m[3]==='(' && !dir) continue;
     a.add(m[2]);
