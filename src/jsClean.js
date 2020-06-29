6db77add0952cabb66fe2b923e04f0156c0c48a4
Subhajit Sahu <wolfram77@gmail.com>
2020-06-29 17:22:12 +0530

    :coconut: chutney cannot detect comment in strings

--- src/jsClean.js
+++ src/jsClean.js
@@ -4,9 +4,9 @@ const {EOL} = require('os');
 
 function jsClean(x, ln=false) {
   x = stripComments(x);
+  // x = x.replace(/\s+(\r?\n)/g, EOL);
   x = x.replace(/\/\*\*.*?\*\//gs, '');
-  x = x.replace(/\s+(\r?\n)/g, '$1');
-  x = x.replace(/(\r?\n)\s*(\r?\n)+/g, ln? '$1':'$1$1');
+  x = x.replace(/(\r?\n)\s*(\r?\n)+/g, ln? EOL:EOL+EOL);
   return x.trim()+EOL;
 }
 module.exports = jsClean;
