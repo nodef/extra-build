e028d283babf68d2819db0209d86f481337c584c
Subhajit Sahu <wolfram77@gmail.com>
2020-07-07 09:49:17 +0530

    :octopus: mr anderson vs agent smith -logs

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -4,7 +4,6 @@ function mdLinks(x, dir=false) {
   var a = new Set(), m = null;
   var x = x.replace(/```.*?```/gs, '');
   while((m=RLINK.exec(x))!=null) {
-    console.log(m.slice(1, 4));
     if(m[1]==='!') continue;
     if(m[3]==='(' && !dir) continue;
     a.add(m[2]);
