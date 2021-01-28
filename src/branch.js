25c07700df2994910a95234f9c042c4b52be0fe6
Subhajit Sahu <wolfram77@gmail.com>
2021-01-29 00:24:26 +0530

    :bus: 1.2.34

--- src/branch.js
+++ src/branch.js
@@ -1,9 +1,6 @@
 const cpExec = require('./cpExec');
-const cpExecStr = require('./cpExecStr');
 const execTsc = require('./execTsc');
 const dirFiles = require('./dirFiles');
-const fileSymbol = require('./fileSymbol');
-const standaloneName = require('./standaloneName');
 const scatterOne = require('./branchOne');
 const minify = require('./minify');
 const path = require('path');
@@ -11,29 +8,27 @@ const path = require('path');
 
 function branch(dir, o) {
   var dir = dir||'src';
-  console.log(`Starting branch publish for directory ${dir} ...`);
-  var pth = o.source||path.join(dir, 'index.ts');
-  execTsc(pth, o.tsc);
-  for(var f of dirFiles(dir)) {
+  console.log(`Starting branch publish for ${o.name} ...`);
+  execTsc(o.source, o.tsc);
+  for (var f of dirFiles(dir)) {
     try {
     var pth = path.join(dir, f);
-    var tmp = scatterOne(pth, o);
-    cpExec('npm publish', {cwd: tmp});
-    var standalone = standaloneName(fileSymbol(f), o.symbolRoot);
-    minify(tmp, Object.assign({standalone}, o));
-    cpExec('npm publish', {cwd: tmp});
-    cpExec(`rm -rf ${tmp}`);
+    var p = scatterOne(pth, o);
+    var cwd = path.dirname(p.metadata);
+    cpExec('npm publish', {cwd});
+    minify(tmp, p);
+    cpExec('npm publish', {cwd});
+    cpExec(`rm -rf "${tmp}"`);
     }
     catch(e) { console.error(e); }
     console.log();
   }
   try {
-  standalone = o.standaloneRoot;
-  cpExec('npm pack '+o.name);
-  var tgz = cpExecStr('ls *.tgz');
-  cpExec(`tar -xvf ${tgz} package/ --strip-components=1`);
-  cpExec('rm -rf '+tgz)
-  minify('.', Object.assign({standalone}, o));
+  // cpExec('npm pack '+o.name);
+  // var tgz = cpExecStr('ls *.tgz');
+  // cpExec(`tar -xvf ${tgz} package/ --strip-components=1`);
+  // cpExec('rm -rf '+tgz)
+  minify('.', o);
   cpExec('npm publish');
   }
   catch(e) { console.error(e); }
