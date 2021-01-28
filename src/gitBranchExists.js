c29381bd8f662239b8df265a85f9521befc74082
Subhajit Sahu <wolfram77@gmail.com>
2021-01-28 18:11:31 +0530

    :bus: 1.2.21

--- src/gitBranchExists.js
+++ src/gitBranchExists.js
@@ -5,6 +5,7 @@ const cpExecStr = require('./cpExecStr');
  * Check if a branch exists.
  */
 function gitBranchExists(b) {
+  return true;
   return cpExecStr(`git branch -a | grep 'remotes/origin/${b}'`).trim().length > 0;
 }
 module.exports = gitBranchExists;
