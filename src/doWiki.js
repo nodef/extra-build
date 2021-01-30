74712bf9e65b6fdb14b390bf2cce195c926ebb14
Subhajit Sahu <wolfram77@gmail.com>
2021-01-30 11:56:03 +0530

    :bus: 1.2.71

--- src/doWiki.js
+++ src/doWiki.js
@@ -1,34 +1,36 @@
 const console = require('./console');
 const dirFiles = require('./dirFiles');
 const fileSymbol = require('./fileSymbol');
+const packageName = require('./packageName');
+const symbolName = require('./symbolName');
 const mdSetJsdoc = require('./mdSetJsdoc');
 const mdLinkWikis = require('./mdLinkWikis');
 const mdLinkBasics = require('./mdLinkBasics');
-const packageName = require('./packageName');
 const gitDiffCodeBlocks = require('./gitDiffCodeBlocks')
 const initWiki = require('./initWiki');
 const fs = require('fs');
 const path = require('path');
 
 
-function updateWiki(dir, jsdocs, o) {
-  var dir = dir||'wiki', o = o||{};
+function doWiki(dir, jsdocs, o) {
+  var dir = dir||'wiki';
   var headerHeavy = false;
   console.log(`Updating Wiki ...`);
   initWiki(o.sourceDir, o);
   for(var f of dirFiles(dir)) {
-    var symbol = fileSymbol(f);
-    var name = packageName(symbol, o.nameRoot);
-    var p = path.join(dir, f);
-    var md = fs.readFileSync(p, 'utf8');
+    var base = fileSymbol(f);
+    var name = packageName(base, o.nameRoot);
+    var symbol = symbolName(base, o.symbolRoot);
+    var pth = path.join(dir, f);
+    var md = fs.readFileSync(pth, 'utf8');
     var jsdoc = jsdocs.get(symbol);
-    if(!jsdoc) { console.error(`WikiError: No JSDoc for ${p}`); continue; }
-    var diffCodeBlocks = gitDiffCodeBlocks(p).length>0;
-    var o1 = Object.assign({}, o, {name, symbol, diffCodeBlocks, headerHeavy});
-    md = mdSetJsdoc(md, jsdoc, o1);
-    md = mdLinkWikis(md, o1);
-    md = mdLinkBasics(md, o1);
-    fs.writeFileSync(p, md);
+    if(!jsdoc) { console.error(`WikiError: No JSDoc for ${pth}`); continue; }
+    var diffCodeBlocks = gitDiffCodeBlocks(pth).length>0;
+    var p = Object.assign({}, o, {name, symbol, diffCodeBlocks, headerHeavy});
+    md = mdSetJsdoc(md, jsdoc, p);
+    md = mdLinkWikis(md, p);
+    md = mdLinkBasics(md, p);
+    fs.writeFileSync(pth, md);
   }
 }
-module.exports = updateWiki;
+module.exports = doWiki;
