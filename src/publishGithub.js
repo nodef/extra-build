b0064ad2cfe3baa9360e5c2b996d5d484f64ceac
Subhajit Sahu <wolfram77@gmail.com>
2021-02-01 03:10:11 +0530

    :bus: fix publishGithb()

--- src/publishGithub.js
+++ src/publishGithub.js
@@ -6,7 +6,7 @@ const jsonWrite = require('./jsonWrite');
 
 function publishGithub(o) {
   var m = jsonRead(o.meta);
-  var name = m.name.replace('@', '').replace('/', '.');
+  var name = `@${o.owner}/${m.name.replace('@', '').replace('/', '.')}`;
   var n = Object.assign({}, m, {name});
   jsonWrite(o.meta, n);
   fileWrite(o.npmrc, `registry=https://npm.pkg.github.com/${o.owner}\n`);
