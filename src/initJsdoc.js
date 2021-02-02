644b6876434d972f97852633d5324f729a02233f
Subhajit Sahu <wolfram77@gmail.com>
2021-02-03 01:56:48 +0530

    :bus: fix create gh-pages if not exists

--- src/initJsdoc.js
+++ src/initJsdoc.js
@@ -11,7 +11,8 @@ function initJsdoc(o) {
   var {jsdocDir: cwd} = o;
   console.log(`Creating gh-pages branch ...`);
   cpExec(`git checkout --orphan gh-pages`, {cwd});
-  cpExec(`rm -rf *`, {cwd});
+  cpExec(`git rm -rf .`, {cwd});
+  cpExec(`git clean -fxd`, {cwd});
   cpExec(`touch index.html`, {cwd});
   gitCommit('initial commit', {push: ' --set-upstream origin gh-pages', cwd})
 }
