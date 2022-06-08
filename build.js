5c979177a7b51441b519dc07703789f476fb4f78
Subhajit Sahu <wolfram77@gmail.com>
2022-06-08 16:48:42 +0530

    :mango: simplify build script

--- build.js
+++ build.js
@@ -1,110 +1,67 @@
-const fs = require('fs');
-const javascript = require('extra-javascript-text');
 const build = require('./');
-
 
 const owner  = 'nodef';
 const srcts  = 'index.ts';
 
 
 
-// Is given file a submodule?
-function isSubmodule(pth) {
-  if (/^_|index.ts$/.test(pth)) return false;
-  if (!/\.ts$/.test(pth)) return false;
-  return true;
-}
-
-
-// Get filename keywords for main/sub package.
-function filenameKeywords(fil) {
-  if (fil !== srcts) return [build.symbolname(fil)];
-  return fs.readdirSync('src').filter(isSubmodule).map(build.keywordname);
-}
-
-
-// Get export keywords for main/sub package.
-function exportKeywords(fil) {
-  var txt  = build.readFileText(`src/${fil}`);
-  var exps = javascript.exportSymbols(txt);
-  return exps.map(e => build.symbolname(e.name));
-}
-
-
 // Get keywords for main/sub package.
-function keywords(fil) {
+function keywords(dm) {
   var m = build.readMetadata('.');
-  var s = new Set([...m.keywords, ...filenameKeywords(fil), ...exportKeywords(fil)]);
+  var k = [...dm.values()].map(d => d.name);
+  var s = new Set([...m.keywords, ...k]);
   return Array.from(s);
 }
 
 
-// Generate main output files.
-function generateMain(fil, sym) {
-  var env = sym? {TYPE: 'web'} : null;
-  build.bundleScript(`.build/${srcts}`, {env});
-  if (sym) build.webifyScript(`index.js`,  `index.js`,  {format: 'cjs', symbol: sym});
-  if (sym) build.webifyScript(`index.mjs`, `index.mjs`, {format: 'esm', symbol: sym});
-}
-
-
 // Publish root package to NPM, GitHub.
-function publishRoot(sym, ver) {
+function publishRoot(dm, sym, ver) {
   var _package = build.readDocument('package.json');
   var m = build.readMetadata();
   m.version  = ver;
-  m.keywords = keywords(srcts);
+  m.keywords = keywords(dm);
   if (sym) { m.name += '.web'; }
-  var _readme  = build.readDocument('README.md');
-  var txt = build.readFileText('README.md');
-  if (sym) txt = txt.replace(/\[Files\]\((.*?)\/\)/g, '[Files]($1.web/)');
-  build.writeFileText('README.md', txt);
   build.writeMetadata('.', m);
   build.publish('.');
   try { build.publishGithub('.', owner); }
   catch {}
-  build.writeDocument(_readme);
   build.writeDocument(_package);
 }
 
 
 // Deploy root package to NPM, GitHub.
-function deployRoot(ver) {
-  var m   = build.readMetadata();
-  var sym = build.symbolname(m.name);
-  generateMain(srcts, '');
-  publishRoot('', ver);
-  // generateMain(srcts, sym);
-  // publishRoot(sym, ver);
+function deployRoot(dm, ver) {
+  // generateMain(srcts, '');
+  publishRoot(dm, '', ver);
 }
 
 
 // Deploy root, sub packages to NPM, GitHub.
-function deployAll() {
+function deployAll(dm) {
   var m   = build.readMetadata();
   var ver = build.nextUnpublishedVersion(m.name, m.version);
-  build.exec(`tsc`);
+  // build.exec(`tsc`);
   build.updateGithubRepoDetails();
   build.generateDocs(`src/${srcts}`);
   build.publishDocs();
-  deployRoot(ver);
+  deployRoot(dm, ver);
   // deploySub(ver);
 }
 
 
 // Generate wiki for all exported symbols.
 function generateWiki() {
-  createWikiFiles();
-  generateWikiFiles();
+  // createWikiFiles();
+  // generateWikiFiles();
 }
 
 
-// Update markdowns README, wiki.
-function updateMarkdown(dm) {
+// Update README.
+function updateReadme(dm) {
   var txt = build.readFileText('README.md');
-  build.markdownReindex(txt, dm);
-  build.markdownUpdateLinkReferences(txt, dm, {owner});
-  build.writeFileText('README.md');
+  txt = build.wikiUpdateIndex(txt, dm);
+  txt = build.wikiUpdateLinkReferences(txt, dm, {owner});
+  build.writeFileText('README.md', txt);
 }
 
 
@@ -112,9 +69,8 @@ function main(a) {
   var p  = build.loadDocs([`src/${srcts}`]);
   var ds = p.children.map(build.docsDetails);
   var dm = new Map(ds.map(d => [d.name, d]));
-  if (a[2] === 'deploy') deployAll();
-  else if (a[2] === 'wiki') generateWiki();
-  else if (a[2] === 'markdown') updateMarkdown(dm);
-  else generateMain(srcts, '');
+  if (a[2] === 'deploy') deployAll(dm);
+  else if (a[2] === 'wiki') generateWiki(dm);
+  else if (a[2] === 'readme') updateReadme(dm);
 }
 main(process.argv);
